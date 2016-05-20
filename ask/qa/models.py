64c9fc371bc66ff70d19dc95fcbcd4f0a180c5a5
AlkBur <alkboor@mail.ru>
2016-05-20 23:17:44 +0300

    Added changes

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,7 +5,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager(models.Manager):                                          
+    def new():                                                                  
+        return '1'                                                              
+                                                                                
+    def popular():                                                              
+        return '1'
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
